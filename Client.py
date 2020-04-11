7e3b5c9428d1291ead7f6284dc13f72142a86cd9
Jingherng <jingherng2010@gmail.com>
2020-04-12 02:11:12 +0800

    Added service 1 and quit functionality

--- Client.py
+++ Client.py
@@ -1,39 +1,59 @@
 from Config import *
-import os, sys, socket, logging
+import os, sys, socket
 
 class Client:
     def __init__(self):
         self.UDP_server_IP = "127.0.0.1"
         self.UDP_server_Port = 5005
         self.invocation = [AT_LEAST_ONCE, AT_MOST_ONCE]
-        logging.basicConfig(filename='client.log',level=logging.DEBUG)
     
     def run(self):
-        logging.info('Starting client socket...')
+        print('Starting client socket...')
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         except socket.error as e:
-            logging.error("Failed to create client socket: {}".format(e))
+            print("Failed to create client socket:\n{}".format(e))
             sys.exit()
 
         HOST = 'localhost'
         PORT = 7777
         
+        print('Choose service:\n1: Read content of file. Specify file pathname, offset(in bytes) and no. of bytes.\n')
+        print('2: Insert content into file. Specify file pathname, offset(in bytes) and sequence of bytes to write into file.\n')
+        print('3: Monitor updates of a file.\n')
+
+        data = input('Enter an option and the corressponding data(Enter "q" to quit): ')
+
         while True:
-            msg = input('Enter message to send:')
-            msg = bytes(msg, 'utf-8')
+            msg = bytes(data, 'utf-8')
             try:
                 self.sock.sendto(msg, (HOST, PORT))
             except socket.error as e:
-                logging.error('Error sending message: {}'.format(e))
+                print('Error sending request: {}'.format(e))
 
             
             try:
                 data, address = self.sock.recvfrom(4096)
-                logging.info('Server reply from {}: {}'.format(address, data))
+                data = data.decode('utf-8')
+                if data == 'q':
+                    self.close()
+                    break
+                print('Server reply:\n{}'.format(data))
 
             except socket.error as e:
-                logging.error('Error receiving message: {}'.format(e))
+                print('Error receiving message:\n{}'.format(e))
+
+            data = input('Enter a request (Press q to quit): ')
+
+        return
+
+    def close(self):
+        print('Closing socket...')
+        try:
+            self.sock.close()
+        except socket.error as e:
+            print('Error closing socket:\n{}'.format(e))
+        print('Socket closed...')
 
 if __name__ == "__main__":
     client = Client()
--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-import socket, logging, time, sys
+import socket, time, sys
 
 class Server:
     def __init__(self):
@@ -7,24 +7,23 @@ class Server:
         self.cache = [] # list for last limit requests & responess for at-most-once invocation semantics
         self.monitor = [] # list for monitoring
         self.time = time.process_time()
-        logging.basicConfig(filename='server.log',level=logging.DEBUG)
 
     def run(self):
         try:
             self.sock = socket.socket(socket.AF_INET, # Internet
                                         socket.SOCK_DGRAM) # UDP
         except socket.error as e:
-            logging.error('Failed to create socket {}'.format(e))
+            print('Failed to create socket:\n{}'.format(e))
             sys.exit()
 
         # bind socket to port
         serverAddress = (self.UDP_ip, self.UDP_port)
-        logging.info('Starting server on {} Port {}'.format(self.UDP_ip, self.UDP_port))
+        print('Starting server on {} Port {}...'.format(self.UDP_ip, self.UDP_port))
 
         try:
             self.sock.bind(serverAddress)
         except socket.error as e:
-            logging.error('Socket bind failed {}'.format(e))
+            print('Socket bind failed:\n{}'.format(e))
             sys.exit()
         
 
@@ -35,32 +34,75 @@ class Server:
     # await data from client
     def await(self):
         while True:
-            logging.info('Awaiting data from client...')
+            print('Awaiting data from client...')
             data, address = self.sock.recvfrom(4096)
-            logging.info('Received data from {}: {}'.format(data, address))
-
-            if not data:
-                logging.info('Data not found')
-                break
-
+            data = data.decode('utf-8')
+            print('Received data from {}:\n{}'.format(address, data))
+            if data == 'q':
+                self.sock.sendto( bytes('q', 'utf-8'), address)
+                self.close()
+                return
             self.replyReq(data, address)
 
     def replyReq(self, data, address):
 
         ## processing of data
-        reply = 'SOME FN()'
+        reply = self.processReq(data)
         ## reply variable encoded in bytes utf-8
         reply = bytes(reply, 'utf-8')
         self.sock.sendto(reply, address)
         return
 
     def close(self):
-        logging.info('Closing socket...')
+        print('Closing socket...')
         try:
             self.sock.close()
         except socket.error as e:
-            logging.error('Error closing socket: {}'.format(e))
-        logging.info('Socket closed...')
+            print('Error closing socket:\n{}'.format(e))
+        print('Socket closed...')
+
+    def processReq(self, data):
+        if not data:
+            return 'Request not found.'
+
+        d = data.split()
+        
+        if len(d) == 4:
+            service = d[0]
+            filePathName = d[1]
+            offset = d[2]
+            numBytes = d[3]
+        else:
+            service = d[0]
+            filePathName = d[1]
+            monitorInterval = d[2]
+
+        if service == '1': # Read content of file
+            return self.readFile(filePathName, offset, numBytes)
+        
+        elif service == '2': # Insert content into file
+            return self.insertContent(filePathName, offset, numBytes)
+        
+        elif service == '3': # Monitor updates made to content of specified file
+            return self.monitorFile(filePathName, monitorInterval)
+
+    
+    def readFile(self, filePathName, offset, numBytes):
+        try:
+            f = open(filePathName, 'r')
+            f.seek(int(offset), 0)
+            content = f.read(int(numBytes))
+            f.close()
+            return content
+        except FileNotFoundError as e:
+            return str(e)
+
+    def insertContent(self, filePathName, offset, numBytes):
+        return
+
+    def monitorFile(self, filePathName, monitorInterval):
+        return
+
 
 if __name__ == "__main__":
     server = Server()
